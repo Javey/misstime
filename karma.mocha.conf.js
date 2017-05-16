e3a13f7f68e32f3eb5f1d3b100a7841cfa2b9e89
Javey <jiawei23716@sina.com>
2017-05-16 21:12:55 +0800

    travis ci & saucelabs

--- karma.mocha.conf.js
+++ karma.mocha.conf.js
@@ -2,6 +2,15 @@ const webpack = require('webpack');
 
 process.env.NODE_ENV = 'production';
 
+const customLaunchers = {
+    sl_chrome: {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'Windows 7',
+        version: '35'
+    }
+};
+
 module.exports = function(config) {
     config.set({
         logLevel: config.LOG_INFO,
@@ -38,17 +47,24 @@ module.exports = function(config) {
         plugins: [
             'karma-chrome-launcher',
             'karma-firefox-launcher',
+            'karma-sauce-launcher',
             'karma-mocha',
             'karma-webpack',
         ],
         // browsers: ['Chrome', 'Safari', 'Firefox', 'Opera', 'IE', 'Phantomjs'],
-        browsers: process.env.TRAVIS ? ['Chrome_travis_ci', 'Firefox'] : [],
-        customLaunchers: {
-            Chrome_travis_ci: {
-                base: 'Chrome',
-                flags: ['--no-sandbox']
-            }
+        // browsers: process.env.TRAVIS ? ['Chrome_travis_ci', 'Firefox'] : [],
+        browsers: Object.keys(customLaunchers),
+        // customLaunchers: {
+            // Chrome_travis_ci: {
+                // base: 'Chrome',
+                // flags: ['--no-sandbox']
+            // }
+        // },
+        sauceLabs: {
+            testName: 'Miss Unit Tests'
         },
+        customLaunchers: customLaunchers,
+        reportes: ['dots', 'saucelabs'],
         client: {
             mocha: {
                 reporter: 'html'
