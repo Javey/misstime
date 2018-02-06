f7ce96ba81f81042eecc76a8cae72c34c553ccca
Javey <jiawei23716@sina.com>
2018-02-06 18:14:56 +0800

    feat: export Types

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {createVNode, createCommentVNode, createUnescapeTextVNode} from './vnode';
+import {createVNode, createCommentVNode, createUnescapeTextVNode, Types} from './vnode';
 import {patch} from './vpatch';
 import {render, removeElement} from './vdom';
 import {MountedQueue} from './utils';
@@ -6,11 +6,15 @@ import {toString} from './tostring';
 import {hydrateRoot, hydrate} from './hydration';
 
 export {
-    createVNode as h, patch, render, 
+    createVNode as h, 
+    patch,
+    render, 
     createCommentVNode as hc, 
     createUnescapeTextVNode as hu,
     removeElement as remove,
     MountedQueue,
     toString as renderString,
-    hydrateRoot, hydrate
+    hydrateRoot, 
+    hydrate,
+    Types,
 };
