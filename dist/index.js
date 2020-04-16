6f6824dfa962610bebadbf17732d950259c53869
Javey <jiawei23716@sina.com>
2020-04-16 16:19:45 +0800

    build

--- dist/index.js
+++ dist/index.js
@@ -883,10 +883,16 @@ function createOrHydrateComponentClassOrInstance(vNode, parentDom, mountedQueue,
 function createComponentClassOrInstance(vNode, parentDom, mountedQueue, lastVNode, isRender, parentVNode, isSVG) {
     return createOrHydrateComponentClassOrInstance(vNode, parentDom, mountedQueue, lastVNode, isRender, parentVNode, isSVG, function (instance) {
         var dom = instance.init(lastVNode, vNode);
-        if (parentDom && (!lastVNode ||
-        // maybe we have reused the component and replaced the dom
-        lastVNode.dom !== dom && !dom.parentNode || !dom.parentNode.tagName)) {
-            parentDom.appendChild(dom);
+        if (parentDom) {
+            // for Animate component reuse dom in Intact
+            if (!lastVNode && parentDom._reserve) {
+                lastVNode = parentDom._reserve[vNode.key];
+            }
+            if (!lastVNode ||
+            // maybe we have reused the component and replaced the dom
+            lastVNode.dom !== dom && !dom.parentNode || !dom.parentNode.tagName) {
+                parentDom.appendChild(dom);
+            }
         }
 
         return dom;
